36289ebbfca49a09ecd82f7a7ee5425ed59bf212
AlenaBM <9642444600a@gmail.com>
2021-04-22 00:39:37 +0300

    create slider

--- js/script.js
+++ js/script.js
@@ -1,26 +1,16 @@
 $(document).ready(function() {
-  
-var projectSlider = new Swiper('.project-slider', {
+
+var projectSlider = new Swiper('.projects__slider', {
         loop: false,
         keyboard: {
             enabled: true,
             onlyInViewport: false,
         },
         navigation: {
-            nextEl: '.project__button--next',
-            prevEl: '.project__button--prev',
+            nextEl: '.projects__button--next',
+            prevEl: '.projects__button--prev',
         },
         slidesPerView: 'auto',
-        breakpoints: {
-        // when window width is >= "N"px
-        568: {
-        slidesPerView: 'auto',
-        direction: 'horizontal',
-        },
-        319: {
-        slidesPerView: 4,
-        direction: 'vertical',
-        },
-        } 
+        direction: 'horizontal'
     });
 });
